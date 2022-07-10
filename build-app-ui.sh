d67d72fe4486302ca1bd9edd6de7a04acfb91441
Oscar Callisaya <oreynaldo.cl@gmail.com>
2022-07-10 04:54:15 -0400

    Improved version of images. Created kubernetes for local.

--- build-app-ui.sh
+++ build-app-ui.sh
@@ -1,9 +1,8 @@
 # expected a node v14.20.0
 # build angular app
 IMAGE_UI=oreynaldocl/ang-node-ui
-IMAGE_API=oreynaldocl/ang-node-api
 
-VERSION=1
+VERSION=2
 
 echo $VERSION
 cd app-ui
@@ -13,10 +12,3 @@ docker build -t $IMAGE_UI:$VERSION -f Dockerfile-dev .
 docker tag $IMAGE_UI:$VERSION $IMAGE_UI:latest
 docker push $IMAGE_UI:$VERSION
 docker push $IMAGE_UI:latest
-cd ..
-
-cd api
-docker build -t $IMAGE_API:$VERSION -f Dockerfile-dev .
-docker tag $IMAGE_API:$VERSION $IMAGE_API:latest
-docker push $IMAGE_API:$VERSION
-docker push $IMAGE_API:latest
